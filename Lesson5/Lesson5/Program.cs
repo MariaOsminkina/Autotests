9732b5ccd0d626f116e27b2f9166b245c34cae45
Maria <maria_osminkina@mail.ru>
2019-03-10 23:31:27 +0300

    lesson 5

---
+++ Lesson5/Lesson5/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    public class Rectangle
+    {
+        private int side1;
+        private int side2;
+
+        public Rectangle(int x, int y)
+        {
+            this.side1 = x;
+            this.side2 = y;
+        }
+        
+        public int AreaCalculator()
+        {
+            return this.side1 * this.side2; 
+        }
+
+        public int PerimeterCalculator()
+        {
+            return (this.side1 + this.side2)*2;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isInt(string value)
+            {
+                bool isCorrect = int.TryParse(value, out int res);
+                if (!isCorrect)
+                {
+                    Console.WriteLine("\r\nIncorrect value. Integer is needed.");
+                    Console.ReadLine();
+                }
+                return res;
+            }
+
+
+            Console.Clear();
+
+            Console.WriteLine("\r\nEnter length:");
+            var entered = Console.ReadLine();
+            int x = isInt(entered);
+
+            Console.WriteLine("\r\nEnter width:");
+            entered = Console.ReadLine();
+            int y = isInt(entered);
+
+            Rectangle instance = new Rectangle(x, y);
+            Console.WriteLine("\r\nRectangle area: {0}", instance.AreaCalculator());
+            Console.WriteLine("\r\nRectangle perimeter: {0}", instance.PerimeterCalculator());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson5/Lesson5_2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5_2
+{
+    public class Car
+    {
+        public string name;
+        private CarPart[] parts;
+
+        public Car(string name, CarPart[] parts)
+        {
+            this.name = name;
+            this.parts = parts;
+        }
+
+        public int getPartsPrice ()
+        {
+            int price = 0;
+            for (int i = 0; i < this.parts.Length; i++)
+            {
+                price += this.parts[i].getPrice();
+            }
+
+            return price;
+        }
+    }
+
+    public class CarPart
+    {
+        private string name;
+        private int price;
+
+        public CarPart(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public int getPrice()
+        {
+            return this.price;
+        }
+    }
+
+    public class Calculator
+    {
+        private Car jeep = new Car("JEEP", new CarPart[2] { new CarPart("Wheel1", 123), new CarPart("Wheel2", 234) });
+        private Car opel = new Car("OPEL", new CarPart[2] { new CarPart("Door", 111), new CarPart("Engine", 222) });
+
+        public int getCarPartsPrice (string carName)
+        {
+            if (carName == jeep.name)
+            {
+                return jeep.getPartsPrice();
+            }
+
+            if (carName == opel.name)
+            {
+                return opel.getPartsPrice();
+            }
+
+            return 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("\r\nEnter car name:");
+            var entered = Console.ReadLine();
+
+            int price = new Calculator().getCarPartsPrice(entered);
+
+            Console.WriteLine("\r\nSumm of car parts of {0} = {1}", entered, price);
+
+            Console.ReadLine();
+        }
+    }
+}
